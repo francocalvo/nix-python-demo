cc12530adde38629a3442a17a1dfadd4c01b2261
francocalvo <dev@francocalvo.ar>
2025-01-23 09:08:55 -0300

    create uv workspace and pythonSet

--- flake.nix
+++ flake.nix
@@ -1,10 +1,60 @@
 {
   description = "A very basic flake";
 
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
-    {
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { self, nixpkgs, uv2nix, pyproject-nix, pyproject-build-systems }:
+    let
+      inherit (nixpkgs) lib;
+      pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+
+      # Use Python 3.12 from nixpkgs
+      python = pkgs.python312;
+
+      # Load a uv workspace from a workspace root.
+      workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+
+      # Create package overlay from workspace.
+      overlay = workspace.mkPyprojectOverlay {
+        # Prefer prebuilt binary wheels as a package source.
+        sourcePreference = "wheel"; # or sourcePreference = "sdist";
+        # Optionally customise PEP 508 environment
+        # environ = {
+        #   platform_release = "5.10.65";
+        # };
+      };
+
+      # Customized Python package set combining base pyproject-nix builders with 
+      # build system overlays, workspace-specific uv.lock overlay, and build fixes.
+      # Used for both production and development environments.
+      pythonSet =
+        # Use base package set from pyproject.nix builders
+        (pkgs.callPackage pyproject-nix.build.packages {
+          inherit python;
+        }).overrideScope (lib.composeManyExtensions [
+          pyproject-build-systems.overlays.default
+          overlay
+        ]);
+    in {
 
     };
 }
